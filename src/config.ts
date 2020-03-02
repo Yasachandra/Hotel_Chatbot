31fd5ebf1b5ad938332f1fdf38b9ce35cf676892
Yasachandra Bansal <yasachandra@gmail.com>
2020-03-02 15:39:50 +0530

    Removing keys and endpoint urls

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export default {
-    host:process.env.HOST || "https://sfb-cosmos-db.documents.azure.com:443/",
-    authKey:process.env.AUTH_KEY || "weainSpvRM66osExcAwfUH68rOq3yjgSdb0nDAKHHdRGx3rRFy7QYv3oiscx67F9C0hQ8zPLUU5pQGi8dI4srw==",
+    host:process.env.HOST || "https://abc.azure.com:443/",
+    authKey:process.env.AUTH_KEY || "<auth-key>",
     databaseId:"chatbot_db",
     collectionId:"hotel_bot_table",
-    chitChatQnAURL: "https://procurement-bot-qna.azurewebsites.net/qnamaker/knowledgebases/a6757cab-dc8b-419e-8bc6-366ecb6cbf52/generateAnswer",
-    chitChatQnAAuthorization: "EndpointKey aeecb31e-5b3e-4266-bd57-17b1ceb0d992",
+    chitChatQnAURL: "<chitchat-qna-url>",
+    chitChatQnAAuthorization: "EndpointKey <chitchat-qna-authorization-key>",
     chitChatQnAConfidence: 80,
-    FeedbackLUISURL: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/a6eb6234-fb8c-4206-a22e-503bc095fcb1?verbose=true&timezoneOffset=0&subscription-key=48a159a967d84dc58258e3025a7936da&q=",
+    FeedbackLUISURL: "<feedback-luis-url>",
     FeedbackLUISConfidence: 90
 }
