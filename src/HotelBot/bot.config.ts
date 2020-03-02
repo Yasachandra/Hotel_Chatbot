68baf6225c564249d13a281b53ac072a8fb31cbd
Yasachandra Bansal <yasachandra@gmail.com>
2020-03-02 15:41:07 +0530

    Removed endpoint urls

--- src/HotelBot/bot.config.ts
+++ src/HotelBot/bot.config.ts
@@ -1,6 +1,6 @@
 export default {
-    DispatcherURL: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/69469a45-d3f9-489e-8d6a-a9522bcb9838?verbose=true&timezoneOffset=0&subscription-key=1d33b6c77c724808afdbe73baf7f5dae&q=",
+    DispatcherURL: "<dispatch-endpoint-url>",
     DispatcherConfidence: 60,
-    HotelBotLUISURL: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d2659015-4a77-4f0f-9dd0-51956081d9c4?verbose=true&timezoneOffset=0&subscription-key=48a159a967d84dc58258e3025a7936da&q=",
+    HotelBotLUISURL: "<bot-luis-url>",
     HotelBotLUISConfidence: 60
 }
